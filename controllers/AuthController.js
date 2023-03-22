412e6433f8c0a77ed62ab20e3b2e75d0a8e4585b
oguilima <guilherme.lima@grupomfx.com>
2023-03-22 13:20:13 -0300

    ultimas alterações

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,7 @@
+const User = require('../models/User')
+
+const bcrypt = require('bcryptjs')
+
 module.exports = class AuthController{
     static login(req, res){
         res.render('auth/login')
@@ -5,5 +9,16 @@ module.exports = class AuthController{
 
     static register(req, res){
         res.render('auth/register')
+    }
+
+    static async registerPost(req, res){
+        const {name, email, password, confirmpassword} = req.body
+
+        if(password != confirmpassword){
+            req.flash('message', 'As senhas não conferem, Tente novamente!')
+            res.render('auth/register')
+
+            return
+        }
     }
 }
