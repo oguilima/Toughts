bc21aa3e2b541ca3958273f297e5f9a70d61f34c
oguilima <guilherme.lima@grupomfx.com>
2023-03-23 13:58:17 -0300

    Projeto finalizado

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -1,9 +1,42 @@
 const Tought = require("../models/Tought")
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
+
 module.exports = class ToughtsController {
     static async showToughts(req, res) {
-        res.render('toughts/home')
+
+        let search = ''
+        
+        if(req.query.search){
+            search = req.query.search
+        }
+
+        let order = ""
+
+        if(req.query.order === 'old'){
+            order = "ASC"
+        }else{
+            order = "DESC"
+        }
+
+        const toughtsData = await Tought.findAll({
+            include: User,
+            where: {
+                title: {[Op.like]: `%${search}%`},
+            },
+            order: [['createdAt', order]]
+        })
+
+        const toughts = toughtsData.map((result) => result.get({plain: true}))
+
+        let toughtsQty = toughts.length
+
+        if(toughtsQty === 0){
+            toughtsQty = false
+        }
+
+        res.render('toughts/home', {toughts, search, toughtsQty})
     }
 
     static async dashboard(req, res) {
