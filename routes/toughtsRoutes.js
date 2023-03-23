2e72c49079f372d040c510849cc165ae5045ed6c
oguilima <guilherme.lima@grupomfx.com>
2023-03-23 00:25:25 -0300

    middleware para verificação de autenticação e formulário de cadastro de pensamentos

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const ToughtsController = require('../controllers/ToughtsController')
 
+//helper
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/add', checkAuth, ToughtsController.createTought)
+router.get('/dashboard', checkAuth, ToughtsController.dashboard)
 router.get('/', ToughtsController.showToughts)
 
 module.exports = router
