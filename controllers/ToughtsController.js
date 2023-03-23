9ff34db15f83f23a0b8c3eec4259e6a481a0e9c0
oguilima <guilherme.lima@grupomfx.com>
2023-03-23 13:00:11 -0300

    atualizando pensamentos

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -11,25 +11,25 @@ module.exports = class ToughtsController {
 
         const user = await User.findOne({
             where: {
-              id: userId,
+                id: userId,
             },
             include: Tought,
             plain: true,
-          })
-      
+        })
 
-        if(!user){
+
+        if (!user) {
             res.redirect('/login')
         }
 
-        const toughts = user.Toughts.map((result) => result.dataValues) 
+        const toughts = user.Toughts.map((result) => result.dataValues)
 
         let emptyToughts = false
 
-        toughts.length === 0 ?  emptyToughts = true :  emptyToughts = false
-        
+        toughts.length === 0 ? emptyToughts = true : emptyToughts = false
 
-        res.render('toughts/dashboard', {toughts, emptyToughts})
+
+        res.render('toughts/dashboard', { toughts, emptyToughts })
     }
 
     static createTought(req, res) {
@@ -55,20 +55,50 @@ module.exports = class ToughtsController {
         }
     }
 
-    static async removeTought(req, res){
+    static async removeTought(req, res) {
         const id = req.body.id
         const UserId = req.session.userid
 
-        try{
-            await Tought.destroy({where: {id: id, UserId: UserId}})
+        try {
+            await Tought.destroy({ where: { id: id, UserId: UserId } })
 
             req.flash('message', 'Pensamento removido com sucesso!')
 
             req.session.save(() => {
                 res.redirect('/toughts/dashboard')
             })
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
+    }
+
+    static async updateTought(req, res) {
+        const id = req.params.id
+
+        const tought = await Tought.findOne({ where: { id: id }, raw: true })
+
+        res.render('toughts/edit', { tought })
+    }
+
+    static async updateToughtSave(req, res) {
+        const id = req.body.id
+
+        const tought = {
+            title: req.body.title
+        }
+
+        try {
+            await Tought.update(tought, { where: { id: id } })
+
+            req.flash('message', 'Pensamento atualizado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (err) {
+            console.log(err)
+        }
+
+
     }
 }
