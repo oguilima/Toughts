8af71246758c01f5d073547fc43fd8269c5d00af
oguilima <guilherme.lima@grupomfx.com>
2023-03-23 00:42:41 -0300

    inserindo pensamentos no banco

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -1,16 +1,35 @@
 const Tought = require("../models/Tought")
 const User = require('../models/User')
 
-module.exports = class ToughtsController{
-    static async showToughts(req, res){
+module.exports = class ToughtsController {
+    static async showToughts(req, res) {
         res.render('toughts/home')
     }
 
-    static async dashboard(req, res){
+    static async dashboard(req, res) {
         res.render('toughts/dashboard')
     }
 
-    static createTought(req, res){
+    static createTought(req, res) {
         res.render('toughts/create')
+    }
+
+    static async createToughtSave(req, res) {
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid
+        }
+
+        try {
+            await Tought.create(tought)
+
+            req.flash('message', 'Pensamento criado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
